2c3c931fa906f8d10d16bbf08d0c784c59182eab
Suzan Chandra <suzanchandra@Suzans-Mac-mini.local>
2026-02-21 12:54:44 +0600

    users data update, delete and data collection set

--- index.js
+++ index.js
@@ -72,11 +72,40 @@ async function run() {
     });
 
     // user collection
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const newUser = req.body;
 
       console.log(newUser);
       const result = await usersCollection.insertOne(newUser);
+
+      res.send(result);
+    });
+
+    app.patch("/users", async (req, res) => {
+      const { email, lastSignInTime } = req.body;
+
+      const filter = { email: email };
+      const updateDoc = {
+        $set: {
+          lastSignInTime: lastSignInTime,
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
 
       res.send(result);
     });
